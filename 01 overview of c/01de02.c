64196e554e2d544f44d48538b4b7851329ba5af5
Mohammad Fatin Nur <119798146+FatinNur@users.noreply.github.com>
2022-12-21 12:58:02 +0600

    Update 01de02.c

--- 01 overview of c/01de02.c
+++ 01 overview of c/01de02.c
@@ -1,2 +1,21 @@
 // Debugging Exercise NO-1.2
 
+/*
+#include (stdio.h) <- wrong parentheses
+SOLUTION: In next line
+*/
+#include<stdio.h>
+
+/*
+void main(void) <- wrong main function return type
+SOLUTION: In next line
+*/
+
+int main(void)
+{
+    /*
+    print("Hello C"); <- wrong function 
+    SOLUTION: In next line
+    */
+    printf("Hello C");
+}
