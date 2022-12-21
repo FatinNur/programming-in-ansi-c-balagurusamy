b7ceaf755a24074ca4cddeef7f17cc7f5b5bc3d8
Mohammad Fatin Nur <119798146+FatinNur@users.noreply.github.com>
2022-12-21 13:03:30 +0600

    Create 01de03.c

---
+++ 01 overview of c/01de03.c
@@ -0,0 +1,48 @@
+// Debugging Exercise NO-1.3
+
+/*
+PROBLEM:	Missing pound sign & capital 'I' -> Include <math.h>
+FIX:		Next line
+*/
+#include <math.h>
+
+/*
+PROBLEM:	stdio.h include is missing. unable to print to screen!
+FIX:		Next line
+*/
+#include <stdio.h>
+
+/*
+PROBLEM:	missing 'int'. wrong type of parenthesis -> main {}
+FIX:		Next line
+*/
+int main()
+{
+	/*
+	PROBLEM:	ISO C90 forbids mixing declarations and code
+			[-Werror,-Wdeclaration-after-statement]
+	FIX:		Next line (Y declaration moved above X = 2.5 statement)
+	*/
+	float Y;
+	float X;
+
+	/*
+	PROBLEM:	Missing variable type identifier -> Y = ex(x);
+			wrong capitalization -> ex(x);
+	FIX:		Next line
+	*/
+	X = 2.5;
+
+	/*
+	PROBLEM:	ISO C90 forbids mixing declarations and code
+			-> float Y = exp( X );
+	FIX:		Next line
+	*/
+        Y = exp(X);
+	/*
+	PROBLEM:	Wrong printf function name && wrong format
+			&& wrong capitalization -> Print(x,y);
+	FIX:		Next line
+	*/
+	printf("%f, %f", X, Y);
+}
